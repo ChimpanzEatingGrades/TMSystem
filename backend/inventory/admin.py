b62009f49d2138fc56a0459afc7bf0bdf2f79b08
ChimpanzEatingGrades <dsesfandiary@addu.edu.ph>
2025-11-17 01:07:31 +0800

    Menu pt 1

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,90 +1,90 @@
-# inventory/admin.py
-from django.contrib import admin
-from .models import RawMaterial, Recipe, RecipeItem, MenuCategory, MenuItem, CustomerOrder, OrderItem, Branch
-from .models import MenuItemBranchAvailability
+# # inventory/admin.py
+# from django.contrib import admin
+# from .models import RawMaterial, Recipe, RecipeItem, MenuCategory, MenuItem, CustomerOrder, OrderItem, Branch
+# from .models import MenuItemBranchAvailability
 
-@admin.register(RawMaterial)
-class RawMaterialAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "unit", "total_quantity")
-    search_fields = ("name",)
+# @admin.register(RawMaterial)
+# class RawMaterialAdmin(admin.ModelAdmin):
+#     list_display = ("id", "name", "unit", "total_quantity")
+#     search_fields = ("name",)
 
-    def total_quantity(self, obj):
-        return obj.get_total_quantity()
-    total_quantity.short_description = "Total Qty"
-
-
-class RecipeItemInline(admin.TabularInline):
-    model = RecipeItem
-    extra = 1
-    autocomplete_fields = ["raw_material"]  # optional; ensure raw_material admin has search_fields
+#     def total_quantity(self, obj):
+#         return obj.get_total_quantity()
+#     total_quantity.short_description = "Total Qty"
 
 
-@admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "yield_quantity", "yield_uom", "created_at")
-    search_fields = ("name",)
-    inlines = [RecipeItemInline]
+# class RecipeItemInline(admin.TabularInline):
+#     model = RecipeItem
+#     extra = 1
+#     autocomplete_fields = ["raw_material"]  # optional; ensure raw_material admin has search_fields
 
 
-@admin.register(MenuCategory)
-class MenuCategoryAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+# @admin.register(Recipe)
+# class RecipeAdmin(admin.ModelAdmin):
+#     list_display = ("id", "name", "yield_quantity", "yield_uom", "created_at")
+#     search_fields = ("name",)
+#     inlines = [RecipeItemInline]
 
 
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = (
-        "id", "name", "picture", "description",
-        "recipe", "recipe_id", "category",
-        "created_at", "updated_at",
-    )
-    search_fields = ("name", "description")
-    list_filter = ("category",)  # <-- removed "is_active"
+# @admin.register(MenuCategory)
+# class MenuCategoryAdmin(admin.ModelAdmin):
+#     search_fields = ("name",)
 
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
-    readonly_fields = ('total_price',)
+# @admin.register(MenuItem)
+# class MenuItemAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "id", "name", "picture", "description",
+#         "recipe", "recipe_id", "category",
+#         "created_at", "updated_at",
+#     )
+#     search_fields = ("name", "description")
+#     list_filter = ("category",)  # <-- removed "is_active"
 
 
-@admin.register(CustomerOrder)
-class CustomerOrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "customer_name", "status", "total_amount", "order_date", "processed_by_name")
-    search_fields = ("customer_name", "customer_phone", "customer_email")
-    list_filter = ("status", "order_date", "processed_by")
-    readonly_fields = ("order_date", "updated_at", "total_amount")
-    inlines = [OrderItemInline]
+# class OrderItemInline(admin.TabularInline):
+#     model = OrderItem
+#     extra = 0
+#     readonly_fields = ('total_price',)
+
+
+# @admin.register(CustomerOrder)
+# class CustomerOrderAdmin(admin.ModelAdmin):
+#     list_display = ("id", "customer_name", "status", "total_amount", "order_date", "processed_by_name")
+#     search_fields = ("customer_name", "customer_phone", "customer_email")
+#     list_filter = ("status", "order_date", "processed_by")
+#     readonly_fields = ("order_date", "updated_at", "total_amount")
+#     inlines = [OrderItemInline]
     
-    fieldsets = (
-        ('Customer Information', {
-            'fields': ('customer_name', 'customer_phone', 'customer_email', 'special_requests')
-        }),
-        ('Order Details', {
-            'fields': ('status', 'subtotal', 'tax_amount', 'total_amount', 'order_date', 'updated_at')
-        }),
-        ('Staff Information', {
-            'fields': ('processed_by', 'processed_by_name', 'notes')
-        }),
-    )
+#     fieldsets = (
+#         ('Customer Information', {
+#             'fields': ('customer_name', 'customer_phone', 'customer_email', 'special_requests')
+#         }),
+#         ('Order Details', {
+#             'fields': ('status', 'subtotal', 'tax_amount', 'total_amount', 'order_date', 'updated_at')
+#         }),
+#         ('Staff Information', {
+#             'fields': ('processed_by', 'processed_by_name', 'notes')
+#         }),
+#     )
 
 
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ("id", "order", "menu_item", "quantity", "unit_price", "total_price")
-    search_fields = ("order__customer_name", "menu_item__name")
-    list_filter = ("order__status", "menu_item__category")
-    readonly_fields = ('total_price',)
+# @admin.register(OrderItem)
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_display = ("id", "order", "menu_item", "quantity", "unit_price", "total_price")
+#     search_fields = ("order__customer_name", "menu_item__name")
+#     list_filter = ("order__status", "menu_item__category")
+#     readonly_fields = ('total_price',)
 
 
-@admin.register(Branch)
-class BranchAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    search_fields = ("name",)
+# @admin.register(Branch)
+# class BranchAdmin(admin.ModelAdmin):
+#     list_display = ("id", "name")
+#     search_fields = ("name",)
 
 
-@admin.register(MenuItemBranchAvailability)
-class MenuItemBranchAvailabilityAdmin(admin.ModelAdmin):
-    list_display = ("menu_item", "branch", "price", "valid_from", "valid_until", "available_from", "available_to", "is_active")
-    list_filter = ("branch", "is_active")
-    search_fields = ("menu_item__name", "branch__name")
+# @admin.register(MenuItemBranchAvailability)
+# class MenuItemBranchAvailabilityAdmin(admin.ModelAdmin):
+#     list_display = ("menu_item", "branch", "price", "valid_from", "valid_until", "available_from", "available_to", "is_active")
+#     list_filter = ("branch", "is_active")
+#     search_fields = ("menu_item__name", "branch__name")
